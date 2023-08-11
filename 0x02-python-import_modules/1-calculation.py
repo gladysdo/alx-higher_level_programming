dd046d252f8310414fe5c6ea8da9757d58e9ec26
gladysdo <doe.glad@gmail.com>
2023-08-10 17:23:44 -0700

    imports functions from the file

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
+    """Print the sum,subtraction,multiple and divide of 10 and 5."""
+    from calculator_1 import add, sub, mul, div
+
     a = 10
     b = 5
-    from calculator_1 import add, subtract, multiply, divide
 
-    sum_add = add(a, b)
-    sub_result = subtract(a, b)
-    mult_result = multiply(a, b)
-    d_result = divide(a, b)
-
-    print("{} + {} = {}".format(sum_add))
-    print("{} -{} = {}".format(sub_result))
-    print("{} * {} = {}".format(mult_result))
-    print("{} / {} = {}".format(d_result))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
