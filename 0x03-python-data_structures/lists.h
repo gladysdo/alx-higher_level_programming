a67b7a935f3e7ddc952b95ccc0b370e7c1134977
gladysdo <doe.glad@gmail.com>
2023-08-14 15:26:18 -0700

    Lists of lists = Matrix

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,11 +1,12 @@
 # ifndef LISTS.H
 # define LISTS.H
 
-def print_list_integer(my_list=[]) :
-def element_at(my_list, idx) :
-def replace_in_list(my_list, idx, element) :
-def print_reversed_list_integer(my_list=[]) :
-def new_in_list(my_list, idx, element) :
-def no_c(my_string) :
+def print_list_integer(my_list=[]);
+def element_at(my_list, idx);
+def replace_in_list(my_list, idx, element);
+def print_reversed_list_integer(my_list=[]);
+def new_in_list(my_list, idx, element);
+def no_c(my_string);
+def print_matrix_integer(matrix=[[]]);
 
 # endif
