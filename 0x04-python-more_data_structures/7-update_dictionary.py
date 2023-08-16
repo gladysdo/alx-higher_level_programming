00b3da5ed93a141b3bf1a986db7f14a9e54925e0
gladysdo <doe.glad@gmail.com>
2023-08-16 11:06:41 -0700

    Multiply by 2

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+my_dict = {}
+update_dictionary(my_dict,"{}")
+print(my_dict)
+update_dictionary(my_dict,"{}")
+print(my_dict)
