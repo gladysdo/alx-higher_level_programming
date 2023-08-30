be82057ccec3fef1de2a7085681de0942d47ce46
gladysdo <doe.glad@gmail.com>
2023-08-30 15:15:08 -0700

    Raise exception

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -3,6 +3,6 @@ def raise_exception():
     a = "This is a string"
     b = 10
     try:
-        result = a + b  # This will raise a TypeError since you can't concatenate a string and an integer
+        result = a + b
     except Exception:
         raise TypeError("This is a custom type exception")
