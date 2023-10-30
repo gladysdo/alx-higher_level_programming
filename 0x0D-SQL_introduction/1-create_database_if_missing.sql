cfa08b8e36c94309de56618fefa2b296397b6b2e
gladysdo <doe.glad@gmail.com>
2023-10-30 06:46:38 -0700

    Create a database

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
---creates the database hbtn_0c_0 in your MySQL server
-CREATE DATABASE IF NOT EXIT 'hbtn_0c_0';
+-- Creates the database hbtn_0c_0 in your MySQL server
+CREATE  Database IF NOT EXISTS  hbtn_0c_0;
