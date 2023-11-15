09adaf25e3b7725609f8833192e0e4ff0046d60a
root <doe.glad@gmail.com>
2023-11-15 08:13:47 -0800

    Rectangle #1

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -2,9 +2,9 @@
 // constructor must take 2 arguments w and h
 
 class Rectangle {
-  constructor(w, h){
-  this.width = w;
-  this.height = h;
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
   }
 }
 module.exports = Rectangle;
