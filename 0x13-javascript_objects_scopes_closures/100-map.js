511746e4644bcda268447d0931b8b3218e7e1e74
root <doe.glad@gmail.com>
2023-11-15 14:07:17 -0800

    Factor index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,10 +1,4 @@
 #!/usr/bin/node
-
-const list = require('./100-data');
-
-const newList = list.map((value, index)  {
-  return value * index;
-});
-
+const list = require('./100-data.js').list;
 console.log(list);
-console.log(newList);
+console.log(list.map((x, xI) => x * xI));
